aca249f2a5a4f4f5ec35b7dd102531da0afd6606
심예은/인턴사원/TV Broadcast Solution팀(yeeun.shim@lge.com) <yeeun.shim@lge.com>
2022-06-08 11:04:39 +0900

    Add Merge

---
+++ mergeVideo/mergeVideo/Merge.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include "Merge.h"
+
+Merge::Merge() {}
+
+Merge::Merge(std::string filePath1, std::string filePath2, std::string savePath) {
+	m_filePath1 = filePath1;
+	m_filePath2 = filePath2;
+	m_savePath = savePath;
+}
+
+void Merge::makeTXTFile() {
+	std::ofstream writeFile("list.txt");
+	writeFile << "file '" << m_filePath1 + "'";
+	writeFile << std::endl;
+	writeFile << "file '" << m_filePath2 + "'";
+	writeFile.close();
+}
+
+bool Merge::isConverted() {
+	if (std::filesystem::path(m_filePath1).extension().compare(std::filesystem::path(m_filePath2).extension()) != 0) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+int Merge::merge() {
+	makeTXTFile();
+	if (isConverted()) {
+		std::string command = "ffmpeg -f concat -safe 0 -i list.txt -c copy -safe 0 " + m_savePath;
+		system(command.c_str());
+		return SUCCESS_MERGE;
+	}
+	else {
+		return IS_NOT_CONVERTED;
+	}
+}
